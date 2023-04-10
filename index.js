04924ae0292924b96af3c0812bf85ea551623f18
Kevin Marin <kevin.concha@correounivalle.edu.co>
2023-04-10 15:24:40 -0500

    Desarrollo: commit inicial

---
+++ index.js
@@ -0,0 +1,34 @@
+//Js template
+
+const express = require('express'); 
+const app = express();
+const port = 3000;
+
+
+
+//este es un cambio en el repo
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static('public'));
+
+app.use(express.static('Images'));
+
+app.use(express.static('scripts'));
+
+
+//routes
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('');
+});
+
+//app.post('/userSingup', rutas);
+
+
+const server = app.listen(port, () => {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("Mande App listening at http://", host, port)
+});
